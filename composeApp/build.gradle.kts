cee059d7bb5bf14c78cbd1f9eda20bb406a96447
sampaiofa-tech <sampaiofa@gmail.com>
2026-09-07 12:45:20 -0300

    test(android): habilita isReturnDefaultValues para mocks do Android SDK em testes unitarios JVM

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -245,7 +245,12 @@ extensions.configure<com.android.build.api.dsl.ApplicationExtension> {
         buildConfig = true
     }
 
-    testOptions { unitTests { isIncludeAndroidResources = true } }
+    testOptions {
+        unitTests {
+            isIncludeAndroidResources = true
+            isReturnDefaultValues = true
+        }
+    }
     dependenciesInfo {
         includeInApk = false
         includeInBundle = true
